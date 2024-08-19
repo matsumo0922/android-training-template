95cd6b7dcaa4b6b97bd04657e437d485ab71eed0
matsumo0922 <caios.system@gmail.com>
2024-08-19 14:31:55 +0900

    add named annotations to module

--- api/src/main/java/jp/co/yumemi/api/di/ApiModule.kt
+++ api/src/main/java/jp/co/yumemi/api/di/ApiModule.kt
@@ -2,7 +2,9 @@ package jp.co.yumemi.api.di
 
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Named
 
 @Module
+@Named("ApiModule")
 @ComponentScan("jp.co.yumemi.api")
 class ApiModule
--- app/src/main/java/jp/co/yumemi/droidtraining/di/AppModule.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/di/AppModule.kt
@@ -2,7 +2,9 @@ package jp.co.yumemi.droidtraining.di
 
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Named
 
 @Module
+@Named("AppModule")
 @ComponentScan("jp.co.yumemi.droidtraining")
 class AppModule
