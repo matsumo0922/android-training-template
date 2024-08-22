5af2a2480033f5d755e4e11628a648446bcc2da9
matsumo0922 <caios.system@gmail.com>
2024-08-22 16:11:13 +0900

    add kotest-koin dependency and use it

--- core/datasource/src/test/java/jp/co/yumemi/droidtraining/core/datasource/YumemiWeatherSourceTest.kt
+++ core/datasource/src/test/java/jp/co/yumemi/droidtraining/core/datasource/YumemiWeatherSourceTest.kt
@@ -1,36 +1,40 @@
 package jp.co.yumemi.droidtraining.core.datasource
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.koin.KoinExtension
 import io.kotest.matchers.shouldBe
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.headersOf
+import io.ktor.serialization.kotlinx.json.json
+import jp.co.yumemi.droidtraining.core.datasource.di.DataSourceModule
 import jp.co.yumemi.droidtraining.core.model.Area
 import jp.co.yumemi.droidtraining.core.model.YumemiConfig
 import kotlinx.serialization.json.Json
+import org.koin.core.component.get
+import org.koin.ksp.generated.module
 import org.koin.test.KoinTest
-import org.koin.test.inject
 
 class YumemiWeatherSourceTest : FunSpec(), KoinTest {
 
-    private val formatter by inject<Json>()
-    private val config by inject<YumemiConfig>()
+    override fun extensions() = listOf(KoinExtension(DataSourceModule().module))
 
     init {
         test("fetchWeather should return expected WeatherDetailEntity") {
             // Arrange
             val response = loadResource("dummy_weather_detail.json")
             val client = setupMockClient(response)
-            val source = YumemiWeatherSource(client, config)
+            val source = YumemiWeatherSource(client, YumemiConfig.DEFAULT)
 
             // Act
             val result = source.fetchWeather(Area.TOKYO)
 
             // Assert
-            result shouldBe formatter.decodeFromString(response)
+            result shouldBe get<Json>().decodeFromString(response)
         }
     }
 
@@ -49,6 +53,10 @@ class YumemiWeatherSourceTest : FunSpec(), KoinTest {
             )
         }
 
-        return HttpClient(engine)
+        return HttpClient(engine) {
+            install(ContentNegotiation) {
+                json(get())
+            }
+        }
     }
 }
--- core/model/src/main/java/jp/co/yumemi/droidtraining/core/model/YumemiConfig.kt
+++ core/model/src/main/java/jp/co/yumemi/droidtraining/core/model/YumemiConfig.kt
@@ -2,4 +2,10 @@ package jp.co.yumemi.droidtraining.core.model
 
 data class YumemiConfig(
     val openWeatherMapApiKey: String,
-)
+) {
+    companion object {
+        val DEFAULT = YumemiConfig(
+            openWeatherMapApiKey = ""
+        )
+    }
+}
