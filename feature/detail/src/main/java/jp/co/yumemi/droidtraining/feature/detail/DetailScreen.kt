c547f78cdb54ce792feb44e65cefbe95c41fa63b
matsumo0922 <caios.system@gmail.com>
2024-08-23 19:28:07 +0900

    add error dialog

--- feature/detail/src/main/java/jp/co/yumemi/droidtraining/feature/detail/DetailScreen.kt
+++ feature/detail/src/main/java/jp/co/yumemi/droidtraining/feature/detail/DetailScreen.kt
@@ -7,19 +7,19 @@ import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import jp.co.yumemi.droidtraining.core.model.Area
 import jp.co.yumemi.droidtraining.core.ui.components.LoadingScreen
+import jp.co.yumemi.droidtraining.core.ui.components.SimpleAlertDialog
 import jp.co.yumemi.droidtraining.feature.detail.components.DetailIdleContent
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun DetailScreen(
     area: Area,
@@ -56,7 +56,15 @@ internal fun DetailScreen(
             }
 
             is DetailScreenState.Error -> {
-                // TODO: 2024/08/23 daichi-matsumoto implement error dialog
+                SimpleAlertDialog(
+                    title = stringResource(R.string.error_title_common),
+                    message = stringResource(R.string.error_message_common),
+                    positiveButtonText = stringResource(R.string.main_weather_action_reload),
+                    negativeButtonText = stringResource(R.string.close),
+                    onPositiveButtonClick = viewModel::fetch,
+                    onNegativeButtonClick = terminate,
+                    onDismissRequest = terminate,
+                )
             }
         }
     }
