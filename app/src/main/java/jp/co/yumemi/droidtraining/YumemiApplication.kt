963b0fea936df3d503e6475de43f1cbc7d9aaaae
matsumo0922 <caios.system@gmail.com>
2024-08-23 10:52:16 +0900

    add timber dependency

--- app/src/main/java/jp/co/yumemi/droidtraining/YumemiApplication.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/YumemiApplication.kt
@@ -1,15 +1,19 @@
 package jp.co.yumemi.droidtraining
 
 import android.app.Application
+import jp.co.yumemi.droidtraining.core.common.YumemiDebugTree
 import jp.co.yumemi.droidtraining.di.applyModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class YumemiApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(YumemiDebugTree())
 
         startKoin {
             androidContext(this@YumemiApplication)
