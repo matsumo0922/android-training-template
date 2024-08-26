25a588465dd08395ededeffec32a94b1f6234a8d
daichi-matsumoto <56629437+matsumo0922@users.noreply.github.com>
2024-08-26 11:32:21 +0900

    Revert "Add datasource and repository test for details api"

--- core/repository/src/test/java/jp/co/yumemi/droidtraining/core/repository/YumemiWeatherRepositoryTest.kt
+++ core/repository/src/test/java/jp/co/yumemi/droidtraining/core/repository/YumemiWeatherRepositoryTest.kt
@@ -1,15 +1,12 @@
 package jp.co.yumemi.droidtraining.core.repository
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import jp.co.yumemi.droidtraining.core.datasource.YumemiWeatherSource
 import jp.co.yumemi.droidtraining.core.model.Area
-import jp.co.yumemi.droidtraining.core.model.WeatherForecast
 import jp.co.yumemi.droidtraining.core.model.entity.WeatherDetailEntity
-import jp.co.yumemi.droidtraining.core.model.entity.WeatherForecastEntity
 import jp.co.yumemi.droidtraining.core.repository.mapper.WeatherDetailMapper
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
@@ -20,8 +17,6 @@ class YumemiWeatherRepositoryTest : FunSpec(), KoinTest {
     private val weatherDetailMapper = mockk<WeatherDetailMapper>()
     private val weatherSource = mockk<YumemiWeatherSource>()
     private val dummyWeatherDetailEntity = mockk<WeatherDetailEntity>()
-    private val dummyWeatherForecastEntity = mockk<WeatherForecastEntity>()
-    private val dummyWeatherForecast = mockk<WeatherForecast>()
     private val testDispatcher = StandardTestDispatcher()
 
     init {
@@ -41,41 +36,6 @@ class YumemiWeatherRepositoryTest : FunSpec(), KoinTest {
             // Assert
             coVerify { weatherDetailMapper.asWeatherDetail(dummyWeatherDetailEntity) }
             coVerify { weatherSource.fetchWeather(area) }
-        }
-
-        test("fetchWeatherForecast should call datasource and mapper") {
-            // Arrange
-            val area = Area.TOKYO
-            val repository = YumemiWeatherRepositoryImpl(weatherSource, weatherDetailMapper, testDispatcher)
-
-            coEvery { weatherDetailMapper.asWeatherForecast(dummyWeatherForecastEntity) } returns mockk()
-            coEvery { weatherSource.fetchWeatherForecast(area) } returns dummyWeatherForecastEntity
-
-            // Act
-            runTest(testDispatcher) {
-                repository.fetchWeatherForecast(area)
-            }
-
-            // Assert
-            coVerify { weatherDetailMapper.asWeatherForecast(dummyWeatherForecastEntity) }
-            coVerify { weatherSource.fetchWeatherForecast(area) }
-        }
-
-        test("fetchWeatherForecast should return the same value as the mapper") {
-            // Arrange
-            val area = Area.TOKYO
-            val repository = YumemiWeatherRepositoryImpl(weatherSource, weatherDetailMapper, testDispatcher)
-
-            coEvery { weatherDetailMapper.asWeatherForecast(dummyWeatherForecastEntity) } returns dummyWeatherForecast
-            coEvery { weatherSource.fetchWeatherForecast(area) } returns dummyWeatherForecastEntity
-
-            runTest(testDispatcher) {
-                // Act
-                val result = repository.fetchWeatherForecast(area)
-
-                // Assert
-                result shouldBe dummyWeatherForecast
-            }
         }
     }
 }
