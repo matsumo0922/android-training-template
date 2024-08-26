0d899c3dc15a9b6b27669595ef2c23127de6e40a
matsumo0922 <caios.system@gmail.com>
2024-08-26 12:36:37 +0900

    draw behind the NavigationBar

--- feature/detail/src/main/java/jp/co/yumemi/droidtraining/feature/detail/components/DetailIdleContent.kt
+++ feature/detail/src/main/java/jp/co/yumemi/droidtraining/feature/detail/components/DetailIdleContent.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -39,11 +38,15 @@ internal fun DetailIdleContent(
     ) { padding ->
         LazyColumn(
             modifier = Modifier
-                .padding(padding)
                 .fillMaxSize()
                 .nestedScroll(scrollBehavior.nestedScrollConnection),
             verticalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(16.dp),
+            contentPadding = PaddingValues(
+                top = 16.dp + padding.calculateTopPadding(),
+                bottom = 16.dp + padding.calculateBottomPadding(),
+                start = 16.dp,
+                end = 16.dp,
+            ),
         ) {
             items(
                 items = uiState.weatherForecast.dayWeathers,
